f6dc25e35e0745b77c2619dc826f99e28493d298
jtreagor <jtreagor@gmail.com>
2022-11-01 15:04:41 -0400

    changed to cmd line arguments updated makefile started init function

--- got.cpp
+++ got.cpp
@@ -1,13 +1,43 @@
 #include<stdio.h>
 #include<iostream>
+#include<cstring>
 using namespace std;
 
-int main(){
-    while (true){
-        char command[50];
-        cout << ">";
-        cin.getline(command, 50);
-        cout << endl << command << endl;
+int init(char *name);
+
+int main(int argc, char *argv[]){
+    for(int i=1; i<argc; i++){
+        printf("%d: %s\n", i, argv[i]);
+
+        if(strcmp(argv[i], "init") == 0){
+            if(argc - i <= 1){
+                printf("Too few arguments\n");
+                break;
+            }
+            int retCode = init(argv[++i]);
+            printf("retCode of init = %d\n", retCode);
+        }
     }
+    return 0;
+}
+
+int init(char *name){
+    int retcode;
+    string nameString = string(name);
+
+    printf("Initializing...\n");
+    string cmd = "mkdir " + nameString;
+    retcode = system(cmd.c_str());
+    if(retcode != 0) return retcode;
+    string cwd = "./" + nameString + "/";
+
+    cmd = "mkdir " + cwd + "/.got";
+    retcode = system(cmd.c_str());
+    if(retcode != 0) return retcode;
+
+
+    cwd.append(".got/");
+
+    printf("Initialized.\n");
     return 0;
 }
