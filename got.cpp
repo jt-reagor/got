56ae763be11cc8628cc0f3367fde3b5d9c89b0a4
jtreagor <jtreagor@gmail.com>
2022-11-03 11:31:42 -0400

    comments in got.cpp

--- got.cpp
+++ got.cpp
@@ -4,23 +4,30 @@
 using namespace std;
 
 int init(char *name);
+int init();
 
 int main(int argc, char *argv[]){
     for(int i=1; i<argc; i++){
-        printf("%d: %s\n", i, argv[i]);
+        // printf("%d: %s\n", i, argv[i]);
 
+        //init
         if(strcmp(argv[i], "init") == 0){
-            if(argc - i <= 1){
-                printf("Too few arguments\n");
-                break;
+            if(argc - i > 1){
+                int retcode = init(argv[++i]);
+                printf("retcode of init = %d\n", retcode);
+            }else{
+                int retcode = init();
+                printf("retcode of init = %d\n", retcode);
             }
-            int retCode = init(argv[++i]);
-            printf("retCode of init = %d\n", retCode);
         }
+
+        //put rest of commands here in if statements like "init" above.
     }
     return 0;
 }
 
+// initializes .got folder in new directory
+// TODO: clean up this code and add error handling using the retcodes.
 int init(char *name){
     int retcode;
     string nameString = string(name);
@@ -31,7 +38,31 @@ int init(char *name){
     if(retcode != 0) return retcode;
     string cwd = "./" + nameString + "/";
 
-    cmd = "mkdir " + cwd + "/.got";
+    cmd = "mkdir " + cwd + ".got";
+    retcode = system(cmd.c_str());
+    if(retcode != 0) return retcode;
+
+
+    cwd.append(".got/");
+    cmd = "mkdir " + cwd + "branches";
+    retcode = system(cmd.c_str());
+    cmd = "mkdir " + cwd + "hashes";
+    retcode = system(cmd.c_str());
+    cmd = "touch " + cwd + "log";
+    retcode = system(cmd.c_str());
+
+    printf("Initialized.\n");
+    return 0;
+}
+
+// initializes .got folder in current directory
+int init(){
+    int retcode;
+
+    printf("Initializing...\n");
+    string cwd = "./";
+
+    string cmd = "mkdir " + cwd + ".got";
     retcode = system(cmd.c_str());
     if(retcode != 0) return retcode;
 
